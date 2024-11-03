ea17a6cf819ab5c5c0ca92195ff7dae42c60719c
anuva98 <47033178+anuva98@users.noreply.github.com>
2024-11-03 08:25:50 -0500

    added enum for order status and the test cases (#64)

--- service/models/__init__.py
+++ service/models/__init__.py
@@ -1,3 +1,3 @@
 from .persistent_base import db, DataValidationError, PersistentBase
 from .item import Item
-from .order import Order
+from .order import Order, Order_Status
--- service/models/order.py
+++ service/models/order.py
@@ -1,17 +1,27 @@
 import logging
 from .persistent_base import db, PersistentBase, DataValidationError
 from .item import Item
+from enum import Enum
 
 logger = logging.getLogger("flask.app")
+
+class Order_Status(Enum):
+    """Enumeration of valid order statuses"""
+
+    Created = 'Created'
+    In_Progress = 'In_Progress'
+    Shipped = 'Shipped'
+    Completed = 'Completed'
 
 
 class Order(db.Model, PersistentBase):
     """Class that represents an Order"""
 
     # Table Schema
+
     id = db.Column(db.Integer, primary_key=True)
     customer_name = db.Column(db.String(64), nullable=False)
-    status = db.Column(db.String(32), nullable=False, default="pending")
+    status = db.Column(db.Enum(Order_Status), default=Order_Status.Created, nullable=False)
     items = db.relationship("Item", backref="order", passive_deletes=True)
 
     def __repr__(self):
@@ -19,10 +29,13 @@ class Order(db.Model, PersistentBase):
 
     def serialize(self):
         """Converts an Order into a dictionary"""
+        if not isinstance(self.status, Order_Status):
+            raise DataValidationError(f"Invalid status value '{self.status}' not in Order_Status Enum")
+        
         return {
             "id": self.id,
             "customer_name": self.customer_name,
-            "status": self.status,
+            "status": self.status.value,
             "created_at": self.created_at.isoformat(),
             "updated_at": self.updated_at.isoformat(),
             "items": [item.serialize() for item in self.items],
@@ -32,7 +45,10 @@ class Order(db.Model, PersistentBase):
         """Populates an Order from a dictionary"""
         try:
             self.customer_name = data["customer_name"]
-            self.status = data["status"]
+            try:
+                self.status = Order_Status(data["status"])
+            except ValueError:
+                raise DataValidationError(f"Invalid status value '{data['status']}' not in Order_Status Enum")
             item_list = data.get("items", [])
             for item_data in item_list:
                 item = Item()
