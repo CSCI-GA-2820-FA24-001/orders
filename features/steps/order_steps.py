b3487b314e5619003363fc9cce5aed17a621a94e
Kirei-Nan <nanzhenghan@outlook.com>
2024-11-17 20:21:03 +0000

    order_steps file updation

--- features/steps/order_steps.py
+++ features/steps/order_steps.py
@@ -1,27 +1,39 @@
-"""
-Order Steps
-Steps file for orders.feature
-For information on Waiting until elements are present in the HTML see:
-    https://selenium-python.readthedocs.io/waits.html
-"""
+import requests
 from behave import given
-from compare import expect
+
+# HTTP Return Codes
+HTTP_200_OK = 200
+HTTP_201_CREATED = 201
+HTTP_204_NO_CONTENT = 204
 
 
-@given('the following orders')
+@given("the following orders")
 def step_impl(context):
-    """ Delete all Orders and load new ones """
-    # List of orders from the context
+    """Delete all Orders and load new ones"""
+    # List all orders and delete them one by one
+    rest_endpoint = f"{context.base_url}/orders"
+    context.resp = requests.get(rest_endpoint)
+    assert context.resp.status_code == HTTP_200_OK
+    for order in context.resp.json():
+        context.resp = requests.delete(f"{rest_endpoint}/{order['id']}")
+        assert context.resp.status_code == HTTP_204_NO_CONTENT
+
+    # Load the database with new orders
     for row in context.table:
-        data = {
-            "customer_name": row['customer_name'],
-            "status": row['status'],
-            "items": [{
-                "product_name": row['product_name'],
-                "quantity": int(row['quantity']),
-                "price": float(row['price'])
-            }]
+        payload = {
+            "customer_name": row["customer_name"],
+            "status": row["status"],
+            "items": [
+                {
+                    "product_name": row["product_name"],
+                    "quantity": int(row["quantity"]),
+                    "price": float(row["price"]),
+                }
+            ],
         }
 
-        context.resp = context.app.post('/orders', json=data)
-        expect(context.resp.status_code).to_equal(201)
+        # Debug print to see what we're sending
+        print(f"Sending payload: {payload}")
+
+        context.resp = requests.post(rest_endpoint, json=payload)
+        assert context.resp.status_code == HTTP_201_CREATED
